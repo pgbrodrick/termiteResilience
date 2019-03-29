db233fcae9567c4ad121bee83482bae5a05d603a
pbrodrick <pbrodrick@carnegiescience.edu>
2019-03-29 15:56:32 -0700

    figure 3 complete

--- height_density_plot.py
+++ height_density_plot.py
@@ -28,12 +28,11 @@ def plot_lhist(hist,edge):
   return phist,pedge
 
 
-
 ############################ Figure 3 ################################
 
-plt.figure(figsize=(12,8))
-gs = gridspec.GridSpec(3, 4)
-gs.update(wspace=0.3,hspace=1.0)
+fig = plt.figure(figsize=(12,8))
+#gs = gridspec.GridSpec(3, 4)
+#gs.update(wspace=0.3,hspace=1.0)
 
 subsample_size = 1000
 df = pd.read_csv('bootstrapped_results/mc_results_ss_' + str(subsample_size) + '.csv',sep=',')
@@ -51,7 +50,10 @@ un_treat = un_treat[un_treat != 'nature_reserve']
 un_treat_label = ['Subsistance Agriculture','Communal Grazing', 'Kruger NP','Private Reserve']
 
 ################# 3a
-ax = plt.subplot(gs[:2, :2])
+ax = fig.add_axes([0.07,0.96,0.02,0.02],zorder=1)
+plt.text(0,0,'a',fontweight='bold')
+plt.axis('off')
+ax = fig.add_axes([0.1,0.5,0.4,0.45],zorder=0)
 
 ss = 0.10
 density_bins = np.arange(0.0,2.6,step=ss)
@@ -74,27 +76,32 @@ plt.legend(un_treat_label)
 
 ################# 3b
 
-ax = plt.subplot(gs[:2, 2:])
 
-height_bins = np.arange(0.2,2.2,step=ss)
+key_df = pd.read_csv('polygon_info/polygon_key.csv',sep=',')
+un_treat = np.unique(np.array(key_df['treatment']))
+print(un_treat)
+un_treat_label = ['Subsistance Agriculture','Communal Grazing', 'Kruger NP','Private Reserve']
+
+#ax = plt.subplot(gs[:2, 2:])
+ax = fig.add_axes([0.52,0.96,0.02,0.02],zorder=1)
+plt.text(0,0,'b',fontweight='bold')
+plt.axis('off')
+ax = fig.add_axes([0.55,0.5,0.4,0.45],zorder=0)
+
+height_list = np.load('munged_dat/height_values.npy')
+
+ss = 0.10
+height_bins = np.arange(0.2,3.0,step=ss)
 for n in range(len(un_treat)):
-  
-  l_height = height[treatment == un_treat[n]]
-  l_reps = reps[treatment == un_treat[n]]
-
-  l_height = l_height[l_reps >= float(int(subsample_size)**2)/2.]
-  l_height = l_height[l_height != -9999]
-  l_height = l_height[np.isnan(l_height) == False]
-  l_height = l_height[np.isinf(l_height) == False]
-
-   
-  h,b = np.histogram(l_height,bins=height_bins)
-  b,h = plot_lhist(h,b)
-  plt.plot(h + float(n) / 15. * ss,b)
-  print((un_treat[n],len(l_height)))
+  if (un_treat[n] != 'nature_reserve'):   
+    h,b = np.histogram(height_list[n],bins=height_bins)
+    b,h = plot_lhist(h/np.max(h),b)
+    #b,h = plot_lhist(h,b)
+    plt.plot(h + float(n) / 15. * ss,b)
+    print(un_treat[n])
 
 plt.xlabel('Termite Mound Height [m]')
-plt.ylabel('Frequency')
+plt.ylabel('Relative Frequency')
 
 plt.legend(un_treat_label)
 
@@ -143,12 +150,19 @@ def bin_vals(x,binsize=5):
   return(np.array(out))
 
 
+un_treat = np.unique(treatment)
+un_treat = un_treat[un_treat != 'nature_reserve']
+un_treat_label = ['Subsistance Agriculture','Communal Grazing', 'Kruger NP','Private Reserve']
 
 colors = ['blue','green','red','orange','grey','purple','cyan','brown','black','yellow']
 
 key_df = pd.read_csv('polygon_info/polygon_key.csv',sep=',')
 treatment = np.array(key_df['treatment'])
 polygon_number = np.array(key_df['polygon_number'])
+
+ax = fig.add_axes([0.07,0.39,0.02,0.02],zorder=1)
+plt.text(0,0,'c',fontweight='bold')
+plt.axis('off')
 
 ind = np.zeros(4).astype(int)
 for _t in range(0,len(un_treat)):
@@ -158,7 +172,7 @@ for _t in range(0,len(un_treat)):
     fname = 'roi_output/poly_'+str(loc_poly[m]) + '.0.csv' 
     rk = get_rk_extr(fname)
 
-    ax = plt.subplot(gs[2,_t])
+    ax = fig.add_axes([0.1 + 0.9/float(len(un_treat)) * _t,0.1,0.9/float(len(un_treat))-0.05,0.26],zorder=0)
     if(ind[_t] == 0):
       plt.title(un_treat_label[_t])
       plt.xlabel('Distance [m]')
@@ -167,8 +181,9 @@ for _t in range(0,len(un_treat)):
 
     x = bin_vals(rk[:,0])
     y = bin_vals(rk[:,1])
-    plt.plot([0,250],[0,250],c='black',ls='--',lw=2,zorder=0)
-    plt.plot(x,y,lw=2,c=colors[ind[_t]],zorder=1) 
+    plt.plot([0,250],[0,250],c='red',ls='--',lw=1,zorder=0)
+    #plt.plot(x,y,lw=2,c=colors[ind[_t]],zorder=1) 
+    plt.plot(x,y,lw=1,c='black',zorder=1) 
 
     plt.axis('equal')
     plt.xlim([-5,255])
