a49c234ef5cb19412502894917aad52a33f6b976
pbrodrick <pbrodrick@carnegiescience.edu>
2019-05-10 17:15:33 -0700

    updated ks test

--- ks_test.py
+++ ks_test.py
@@ -53,16 +53,17 @@ reps = np.array(df['rep_poly_count'])
 rem = np.array(df['mean_rem'])
 tch = np.array(df['mean_tch'])
 
-ss = 0.10
-density_bins = np.arange(0.0,2.6,step=ss)
 un_treat = np.unique(treatment)
 un_treat = un_treat[un_treat != 'nature_reserve']
 
-def calc_stat(value,name_key):
+def calc_stat(value,name_key,type=0):
 
     distros = []
     for n in range(len(un_treat)):
-      l_density = value[treatment == un_treat[n]]
+      if (type == 0):
+        l_density = value[treatment == un_treat[n]]
+      else:
+        l_density = value[n]
        
       h,b = np.histogram(l_density,bins=density_bins)
       b,h = plot_lhist(h,b)
@@ -83,8 +84,38 @@ def calc_stat(value,name_key):
     df.to_csv('stats/' + name_key + '_ks_pvalue.csv',sep=',',index=False)
 
 
+ss = 0.10
+density_bins = np.arange(0.0,2.6,step=ss)
 calc_stat(density, 'density')
 calc_stat(tch, 'tch')
+ss = 0.05
+density_bins = np.arange(0.0,1.1,step=ss)
 calc_stat(rem, 'rem')
+
+
+
+
+
+key_bootstrap_df = pd.read_csv('polygon_info/polygon_key.csv',sep=',')
+un_treat = np.unique(np.array(key_bootstrap_df['treatment']))
+un_treat_label = ['Subsistance Agriculture','Communal Grazing', 'Kruger NP','Private Reserve']
+height_list = np.load('munged_dat/height_values.npy')
+
+height_list = height_list[np.arange(len(height_list)) != un_treat.tolist().index('nature_reserve')]
+un_treat = un_treat[un_treat != 'nature_reserve']
+
+
+ss = 0.10
+density_bins = np.arange(0.2,3.0,step=ss)
+calc_stat(height_list, 'height',type=1)
+
+
+
+
+
+
+
+
+
 
 
